b028d24923514476a07738b3ea37acdceb64214d
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-05-30 21:25:27 +0100

    forward the message

--- textnudge.py
+++ textnudge.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask, request
 from twilio.rest import TwilioRestClient
 from twilio import twiml
 
 app = Flask(__name__)
+app.config['PERSON_ONE'] = os.getenv('PERSON_ONE')
+app.config['PERSON_TWO'] = os.getenv('PERSON_TWO')
 client = TwilioRestClient()
 
 
@@ -13,7 +16,22 @@ def hello():
 
 @app.route('/receive', methods=['POST'])
 def receive():
+    message = client.messages.create(
+        to=get_forward_number(request.form),
+        from_=request.form['To'],
+        body=request.form['Yay'])
+    print("{}".format(message))
+
     print("{} {} {}".format(request.method, request.data, request.form))
     r = twiml.Response()
     r.message("You are the bestest!")
     return str(r)
+
+
+def get_forward_number(form):
+    if form['From'] == app.config['PERSON_ONE']:
+        return app.config['PERSON_TWO']
+    elif form['From'] == app.config['PERSON_TWO']:
+        return app.config['PERSON_ONE']
+    else:
+        raise ValueError("Invalid from address {}".format(form['From']))
