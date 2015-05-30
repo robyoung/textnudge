50d4df12aa226ea88c896bb84a02ae55c16d3a6c
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-05-30 20:38:02 +0100

    Reply to received text messages

--- textnudge.py
+++ textnudge.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from twilio.test import TwilioRestClient
+from twilio import twiml
 
 app = Flask(__name__)
+client = TwilioRestClient()
+
 
 @app.route('/')
 def hello():
     return "Hello world!"
+
+
+@app.route('/receive')
+def receive():
+    r = twiml.Response()
+    r.message("You are the bestest!")
+    return str(r)
