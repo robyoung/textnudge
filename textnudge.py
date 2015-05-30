5e1b682cd070c0c19858d04f32c46c18b2a53960
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-05-30 22:15:46 +0100

    tidy up get_forwarded_number

--- textnudge.py
+++ textnudge.py
@@ -16,9 +16,8 @@ def hello():
 
 @app.route('/receive', methods=['POST'])
 def receive():
-    print("{} {} {}".format(request.method, request.data, request.form))
     message = client.messages.create(
-        to=get_forward_number(request.form),
+        to=get_forward_number(request.form['From']),
         from_=request.form['To'],
         body=request.form['Body'])
     print("{}".format(message))
@@ -27,13 +26,13 @@ def receive():
     return str(r)
 
 
-def get_forward_number(form):
-    if form['From'] == app.config['PERSON_ONE']:
+def get_forward_number(from_):
+    if from_ == app.config['PERSON_ONE']:
         return app.config['PERSON_TWO']
-    elif form['From'] == app.config['PERSON_TWO']:
+    elif from_ == app.config['PERSON_TWO']:
         return app.config['PERSON_ONE']
     else:
-        raise ValueError("Invalid from address {}".format(form['From']))
+        raise ValueError("Invalid from address {}".format(from_))
 
 if __name__ == '__main__':
     app.debug = True
